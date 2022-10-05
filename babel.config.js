cd0e5a94f76f07515f616db064e88d44ca5edf55
jamesw <jamersonwalderson@gmail.com>
2022-10-05 14:51:36 -0300

    feat: adicionando pugin babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          '@components': './src/components',
+        },
+      },
+    ],
     'react-native-reanimated/plugin',
-  ]
+  ],
 };
